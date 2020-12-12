c0836485b8ee7b036aa7ee6c9a7f1cd942a1719f
paul_ <paul_@LAPTOP-LR5ETQ3U>
2020-12-12 15:26:24 -0500

    Creacion de atributos, clases y metodos.

---
+++ src/ec/edu/ups/controlador/ControladorFacturaClienteFijo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import ec.edu.ups.modelo.FacturaClienteFijo;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author paul_
+ */
+public class ControladorFacturaClienteFijo extends AbstractControlador<FacturaClienteFijo> {
+
+    public ControladorFacturaClienteFijo(String ruta) {
+        super(ruta);
+    }
+    
+    @Override
+    public boolean validar(FacturaClienteFijo objeto) {
+        return false;
+        
+    }
+
+    @Override
+    public int generarId() {
+        List<FacturaClienteFijo> temp = new ArrayList();
+         super.getLista().stream().map(a -> (FacturaClienteFijo) a).forEachOrdered(m -> {
+             temp.add(m);
+        });
+
+        if (temp.size() > 0 && temp != null) {
+            return temp.get(temp.size() - 1).getId() + 1;
+        } else {
+            return 1;
+        }
+        
+    }
+    
+}
---
+++ src/ec/edu/ups/controlador/ControladorFacturaClienteM.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import ec.edu.ups.modelo.FacturaClienteM;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author paul_
+ */
+public class ControladorFacturaClienteM extends AbstractControlador<FacturaClienteM> {
+
+    public ControladorFacturaClienteM(String ruta) {
+        super(ruta);
+    }
+    
+    @Override
+    public boolean validar(FacturaClienteM objeto) {
+        return false;
+        
+    }
+
+    @Override
+    public int generarId() {
+           List<FacturaClienteM> temp = new ArrayList();
+         super.getLista().stream().map(a -> (FacturaClienteM) a).forEachOrdered(m -> {
+             temp.add(m);
+        });
+
+        if (temp.size() > 0 && temp != null) {
+            return temp.get(temp.size() - 1).getId() + 1;
+        } else {
+            return 1;
+        }
+    }
+}
